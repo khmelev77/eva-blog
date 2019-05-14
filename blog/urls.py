94eb21e602d38e0c245219113b13570f98ccf8b9
Nikita <codeine@MacBook-Air-Nikita.local>
2019-05-14 14:06:22 +0300

    Release

---
+++ blog/urls.py
@@ -0,0 +1,32 @@
+"""evaengine URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.blog, name='blog')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='blog')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', post_lists, name='posts_list_url'),
+    path('post/create/', PostCreate.as_view(), name='post_create_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+    path('post/<str:slug>/update', PostUpdate.as_view(), name='post_update_url'),
+    path('post/<str:slug>/delete', PostDelete.as_view(), name='post_delete_url'),
+
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
+    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
+    path('tag/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url'),
+]
